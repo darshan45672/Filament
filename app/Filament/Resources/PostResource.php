2ed5a70c0e911d3117692b89d0cc6d41db365694
darshan45672 <drshnbhandary@gmail.com>
2024-03-21 11:06:03 +0530

    added post and category panel

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Category;
+use App\Models\Post;
+use Faker\Core\Color;
+use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')->label('Title')->required(),
+                TextInput::make('slug')->label('Slug')->required(),
+
+                Select::make('category_id')->label('Category')->options(
+                    fn () => Category::all()->pluck('name', 'id')
+                )->required(),
+
+                FileUpload::make('thumbnail')->label('Image')->required()->disk('public')->directory('thumbnails'),
+                ColorPicker::make('color')->label('Color')->required(),
+                MarkdownEditor::make('content')->label('Content')->required(),
+                TagsInput::make('tags')->label('Tags')->required(),
+                Checkbox::make('is_published')->label('Is Published'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
